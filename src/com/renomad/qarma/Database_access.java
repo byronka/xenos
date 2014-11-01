a5887ff9627c64e7abaf5c73ca0cd4b4bf31c0c1
Byron Katz <byronka@msn.com>
2014-10-31 23:57:48 -0400

    starting to come together. Moving the file access stuff into its own utilities file and putting proper comments.  Even being precise about number of lines betweeen methods (2).

--- src/com/renomad/qarma/Database_access.java
+++ src/com/renomad/qarma/Database_access.java
@@ -4,32 +4,13 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.File;
+import com.renomad.qarma.File_utilities;
 
 public class Database_access {
 
-  private static String getTextFromFile(String filepath) {
-    FileReader fr = null;
-    File file = new File(filepath);
-    long length_of_array = file.length();
-    char[] buffer = new char[(int)length_of_array+1];
-    try {
-      fr = new FileReader(filepath);
-      fr.read(buffer);
-      fr.close();
-    } catch (FileNotFoundException e) {
-      //handle the error
-    } catch (IOException e) {
-      //handle
-    } catch (Exception e) {
-      //handle
-    }
-     return new String(buffer);
-  }
-
+  /**
+    *Boilerplate code necessary to run the java mysql connector.
+    */
   private static void registerSqlDriver() {
     try {
       // The newInstance() call is a work around for some
@@ -41,32 +22,55 @@ public class Database_access {
     }
   }
 
-  private static void runSqlStatement(String sqlText, String connection_string) {
+  /**
+    *A wrapper for Statement.execute()
+    *
+    * Opens and closes a connection each time it's run.
+    * @param sqlText the SQL text we will run - it must be a
+    *  single statement.  Multiple combined statements will fail.
+    * @param connection_string The connection string we'll use to connect.
+    * @return true if the first result is a ResultSet object; false 
+    *  if it is an update count or there are no results
+    */
+  private static boolean runSqlStatement(
+      String sqlText, 
+      String connection_string) {
+    boolean result = false;
     try {
       Connection conn = DriverManager.getConnection(connection_string);
       Statement stmt = conn.createStatement();
-      stmt.execute(sqlText);
+      result = stmt.execute(sqlText);
       conn.close();
     } catch (SQLException ex) {
       System.out.println("SQLException: " + ex.getMessage());
       System.out.println("SQLState: " + ex.getSQLState());
       System.out.println("VendorError: " + ex.getErrorCode());
     }
+    return result;
   }
+
 
   private static void run_script_from_file_before_db_exists(String filepath) {
-    String sqlText = getTextFromFile(filepath);
-    runSqlStatement(sqlText,"jdbc:mysql://localhost/?user=qarmauser&password=hictstd!");
+    String sqlText = File_utilities.get_text_from_file(filepath);
+    runSqlStatement(
+        sqlText,
+        "jdbc:mysql://localhost/?user=qarmauser&password=hictstd!");
   }
 
+
   public static void run_script_from_file(String filepath) {
-    String sqlText = getTextFromFile(filepath);
-    runSqlStatement(sqlText,"jdbc:mysql://localhost/test?user=qarmauser&password=hictstd!");
+    String sqlText = File_utilities.get_text_from_file(filepath);
+    runSqlStatement(
+        sqlText,
+        "jdbc:mysql://localhost/test?user=qarmauser&password=hictstd!");
   }
+
 
   public static void main(String[] args) {
     registerSqlDriver();
-    run_script_from_file_before_db_exists("/home/byron/dev/byron_qarma/db_scripts/create_database.sql");
-    run_script_from_file("/home/byron/dev/byron_qarma/db_scripts/create_usertable.sql");
+    run_script_from_file_before_db_exists(
+        "/home/byron/dev/byron_qarma/db_scripts/create_database.sql");
+    run_script_from_file(
+        "/home/byron/dev/byron_qarma/db_scripts/create_usertable.sql");
   }
 }
