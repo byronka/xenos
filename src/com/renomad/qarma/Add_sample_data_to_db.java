0694532b7f34701eeba788f04a533a5ac1160e7f
Byron Katz <byron@renomad.com>
2014-12-06 16:44:21 -0500

    renaming sample_data file so it becomes appropriate per version of schema changes

--- src/com/renomad/qarma/Add_sample_data_to_db.java
+++ src/com/renomad/qarma/Add_sample_data_to_db.java
@@ -6,7 +6,7 @@ public class Add_sample_data_to_db {
 
   public static void main(String[] args) {
 		Build_db_schema.run_multiple_statements(
-				"db_scripts/add_sample_data.sql");
+				"db_scripts/v1_add_sample_data.sql");
   }
 
 }
