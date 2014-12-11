a16dad0b85e707ca23e030a1e93ed97c62ded3db
Byron Katz <byron@renomad.com>
2014-12-11 16:35:49 -0500

    removing javadocs directory, and ignoring it, on git.  Adding to build.xml so Ant will now handle javadocs and unit tests.  In adding a precursor test fixture, I will be refactoring code in database_access soon.

---
+++ src/com/renomad/qarma/tests/Request_tests.java
@@ -0,0 +1,16 @@
+package com.renomad.qarma.tests;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
+
+public class Request_tests {
+
+	@Test
+	public void do_stuff() {
+
+	}
+
+}
