0836601889ef8b2a4a8e29c283f517d8409d0a89
Byron Katz <byronka@msn.com>
2015-02-17 22:23:24 -0500

    BK-#77- looking at a framework, httpcomponent, to use for integration testing.  It's lightweight, uses Java, has adequate documentation and full source, and I think (hope) worth the investment.
    Or not...if not we'll just chuck it.

--- src/com/renomad/xenos/tests/Feature_tests.java
+++ src/com/renomad/xenos/tests/Feature_tests.java
@@ -8,12 +8,26 @@ import java.io.PrintWriter;
 import java.io.PrintStream;
 import java.net.URL;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.junit.Test;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import static org.junit.Assert.*;
 
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.impl.client.HttpClients;
 public class Feature_tests {
 
   private HttpURLConnection create_basic_browser() throws Exception {
@@ -60,9 +74,10 @@ public class Feature_tests {
       HttpURLConnection huc = create_basic_browser();
       huc.setRequestMethod("POST");
       huc.setRequestProperty("Accept-Language",culture);
-      huc.setDoOutput(true);
-      OutputStream os = huc.getOutputStream();
-      setStringToPostContent(os, body);
+      huc.setFollowRedirects(false);
+      huc.connect();
+      //OutputStream os = huc.getOutputStream();
+      //setStringToPostContent(os, body);
       InputStream is = huc.getInputStream();
       String response = getString(is);
       return response;
@@ -76,6 +91,47 @@ public class Feature_tests {
   public void testee() {
     String result = do_post("en", "username=bob&password=password");
     System.out.println(result);
+  }
+
+  @Test
+  public void testee_yumiee() throws Exception {
+    HttpGet httpget = new HttpGet("http://localhost:8080");
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+    HttpGet httpGet = new HttpGet("http://localhost:8080");
+    CloseableHttpResponse response1 = httpclient.execute(httpGet);
+    // The underlying HTTP connection is still held by the response object
+    // to allow the response content to be streamed directly from the network socket.
+    // In order to ensure correct deallocation of system resources
+    // the user MUST call CloseableHttpResponse#close() from a finally clause.
+    // Please note that if response content is not fully consumed the underlying
+    // connection cannot be safely re-used and will be shut down and discarded
+    // by the connection manager. 
+    try {
+        System.out.println(response1.getStatusLine());
+        HttpEntity entity1 = response1.getEntity();
+        // do something useful with the response body
+        // and ensure it is fully consumed
+        EntityUtils.consume(entity1);
+    } finally {
+        response1.close();
+    }
+
+    HttpPost httpPost = new HttpPost("http://localhost:8080/login.jsp");
+    List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+    nvps.add(new BasicNameValuePair("username", "bob"));
+    nvps.add(new BasicNameValuePair("password", "password"));
+    httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+    CloseableHttpResponse response2 = httpclient.execute(httpPost);
+
+    try {
+        System.out.println(response2.getStatusLine());
+        HttpEntity entity2 = response2.getEntity();
+        // do something useful with the response body
+        // and ensure it is fully consumed
+        EntityUtils.consume(entity2);
+    } finally {
+        response2.close();
+    }
   }
 
   @Test
