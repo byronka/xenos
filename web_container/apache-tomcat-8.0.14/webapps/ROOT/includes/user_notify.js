9e9d32f661e6655e9bf3cec6c45dadda1a9c1f5a
Byron Katz <byronka@msn.com>
2015-02-26 20:58:19 -0500

    BK-#121- Some sprucing up.

--- web_container/apache-tomcat-8.0.14/webapps/ROOT/includes/user_notify.js
+++ web_container/apache-tomcat-8.0.14/webapps/ROOT/includes/user_notify.js
@@ -1,123 +1,127 @@
 var xenos_user_notify = {};
 
 
-//xenos_user_notify.user_message_handler = function() {
+//Module 1: contains the text of the dialog element.
+// returns an object with a property to call, display,
+// which takes a string and displays it for 3 seconds
+xenos_user_notify.message_displayer = function() {
 
+  //This sets up the actual element, and puts it on the page.
+  var display_dialog = function(text_to_show) {
+    var notification_dialog = 
+      "<div id='notification_dialog' " +                    
+      " style='font-size: 17px;width: 200px; height: 200px;" +
+      "   top: 5px;right:10px; background-color: gold;" +
+      "   z-index: 2; position: fixed;'>" +
+      text_to_show +
+      "</div>";
 
-  //Module 1: simply shows and kills a dialog.
-  //It takes some text that it will display to the user.
-  var message_displayer = function(text_to_show) {
+    document.body.insertAdjacentHTML('afterbegin', notification_dialog);
+    setTimeout(message_hider, 3000);
+  }    
 
-    //This sets up the actual element, and puts it on the page.
-    var display_dialog = function() {
-      var notification_dialog = 
-        "<div id='notification_dialog' " +                    
-        " style='font-size: 20px;width: 200px; height: 120px;" +
-        "   top: 5px;right:10px; background-color: gold;" +
-        "   z-index: 2; position: fixed;'>" +
-        text_to_show +
-        "</div>";
-
-      document.body.insertAdjacentHTML('afterbegin', notification_dialog);
-    }    
-
-    //This fades out the dialog until invisible, then removes it from
-    // the DOM
-    var message_hider = function() {
-      var dialog = document.getElementById('notification_dialog');
-      dialog.style.opacity = 1; 
-      var fade = function() {
-        if ((dialog.style.opacity-=.1) < 0) { 
-          var dialog_kill = document.getElementById('notification_dialog');
-          document.body.removeChild(dialog_kill);
-        } else {
-          setTimeout(fade,40);
-        }
-      };
-      fade(); //kick it off.
-    }
-
-    //This part is the interface
-    return {
-      display: display_dialog,
-      hide: message_hider
+  //This fades out the dialog until invisible, then removes it from
+  // the DOM
+  var message_hider = function() {
+    var dialog = document.getElementById('notification_dialog');
+    dialog.style.opacity = 1; 
+    var fade = function() {
+      if ((dialog.style.opacity-=.1) < 0) { 
+        var dialog_kill = document.getElementById('notification_dialog');
+        document.body.removeChild(dialog_kill);
+      } else {
+        setTimeout(fade,40);
+      }
     };
-
+    fade(); //kick it off.
   }
 
+  //This part is the interface
+  return {
+    display: display_dialog //accepts a string param for text to show
+  };
 
-  //Modele 1a: a message queue.  Takes in new messages and 
-  //displays them in an even, timely manner.
-  var message_queue = function() {
+}
 
-    var the_queue = [];
 
-    var add_new_msg = function(msg) {
-      the_queue.push(msg);
-    };
+//Modele 2: a message queue.  Takes in new messages and 
+//displays them in an even, timely manner.  The way it does
+//this is it has a function that gets run every few seconds,
+//and at that time it tries popping an element off the array
+//to display.
+xenos_user_notify.message_queue = function() {
 
-    var process_queue = function() {
-      while (the_queue.length > 0) {
-        var mytext = the_queue.pop();
-        var displayer = message_displayer(mytext);
-        displayer.display();
-        setTimeout(function(){displayer.hide()}, 3000);
+  var the_queue = [];
+  var displayer = xenos_user_notify.message_displayer();
+
+  var add_new_msg = function(msg) {
+    the_queue.push(msg);
+  };
+
+  //every five seconds, wake up.  If there is something
+  //left on the array, display it.
+  var process_queue = function() {
+    if (the_queue.length > 0) {
+      var mytext = the_queue.pop();
+      displayer.display(mytext);
+    }
+    setTimeout(process_queue,5000);
+  };
+
+  process_queue(); //start up the processing
+
+  return {
+    add_msg: add_new_msg //takes a string param to add on the queue
+  };
+
+};
+
+
+//Module 3: Has the responsibility to speak to the server.
+// if it gets any messages from the server, it will send them
+// to the message queue.
+//returns an object with a method that can be run to call
+// the server for more data.
+xenos_user_notify.message_checker = function() {
+
+  var request = new XMLHttpRequest(); 
+  var mq = xenos_user_notify.message_queue();
+
+  var response_handler = function() {
+      if (request.readyState != 4 || request.status != 200) {
+        return;  //if not the state we want, ignore.
       }
-      setTimeout(process_queue,4000);
-    };
-
-    process_queue(); //start up the processing
-
-    return {
-      add_msg: add_new_msg
-    };
-
+      //otherwise, success!
+      var mydoc = request.responseXML;
+      var text_messages = mydoc.getElementsByTagName('div');
+      for (var index = 0; index < text_messages.length;index++) {
+        var mytext = text_messages[index].innerHTML;
+        mq.add_msg(mytext);
+      }
   };
 
 
-  //Module 2: calls periodically to the server to check
-  // on new messages.  
-  var message_checker = function() {
-
-    var request = new XMLHttpRequest(); 
-    var mq = message_queue();
-
-    var response_handler = function() {
-        if (request.readyState != 4 || request.status != 200) {
-          return;  //if not the state we want, ignore.
-        }
-        //otherwise, success!
-        var mydoc = request.responseXML;
-        var text_messages = mydoc.getElementsByTagName('div');
-        for (var index = 0; index < text_messages.length;index++) {
-          var mytext = text_messages[index].innerHTML;
-          mq.add_msg(mytext);
-        }
-    };
-
-
-    var call_to_server = function() {
-      request.open("GET", "temporary_messages.jsp", true); 
-      request.responseType = "document";
-      request.onreadystatechange = response_handler;
-      request.send();
-    };
-
-    //This part is the interface
-    return {
-      call_server: call_to_server
-    };
-
+  var call_to_server = function() {
+    request.open("GET", "temporary_messages.jsp", true); 
+    request.responseType = "document";
+    request.onreadystatechange = response_handler;
+    request.send();
   };
 
-  var mc = message_checker();
-
-  var run_me = function() {
-    mc.call_server();
-    setTimeout(run_me, 30 * 1000);
+  //This part is the interface
+  return {
+    call_server: call_to_server
   };
 
-  run_me();
+};
 
-//}
+xenos_user_notify.mc = xenos_user_notify.message_checker();
+
+xenos_user_notify.run_me = function() {
+  xenos_user_notify.mc.call_server();
+  setTimeout(xenos_user_notify.run_me, 30 * 1000);
+};
+
+xenos_user_notify.run_me();
+
 
